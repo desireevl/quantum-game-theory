7e771ab7e562f896409ef23b7741d90f7a41ea90
Rajiv Krishnakumar <rajkk1@yahoo.com>
2019-11-18 11:29:03 -0500

    added comments to protocol.py

--- protocols.py
+++ protocols.py
@@ -1,9 +1,12 @@
 from enum import Enum
 
 class Protocol(Enum):
-    
+    """
+    The various different quantum/classical game theory game protocols 
+    """
     EWL = "EWL quantization protocol"
     MW = "MW quantization protocol"
+    Classical = "Classical protocol"
     
     def describe(self):
         # self is the member here
