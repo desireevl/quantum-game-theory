9e6a08d238626026c8bc728eb44391d2c92157ce
rajkk <rajkk1@yahoo.com>
2020-03-31 17:50:06 -0400

    reset test_logic.py

---
+++ backend/tests/test_logic.py
@@ -0,0 +1,15 @@
+from backend.logic import PayoffTable
+
+# User prisoner's dilemma payoff
+sample_payoff_table = {'00': (-1, -1),
+                       '01': (-3, 0),
+                       '10': (0, -3),
+                       '11': (-2, -2)}
+
+def test_PayoffTable():
+    payoff_table = PayoffTable(n_players=2, n_choices=2, payoff=sample_payoff_table)
+    assert payoff_table.get_payoff_table() == sample_payoff_table
+    for key, value in sample_payoff_table.items():
+        assert payoff_table.get_payoffs(key) == value
+        payoff_table.set_payoffs(key, (0, 0))
+        assert payoff_table.get_payoffs(key) == (0, 0)
